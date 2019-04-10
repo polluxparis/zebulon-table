4c5fd3a6115a6908968adcd1f95dc7f95ae26a48
PC1\thomas <thomas.bres@gmail.com>
2019-04-10 19:14:03 +0200

    Updates

---
+++ precache-manifest.8ced2cc76292c3e282aa273eb9a2536c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf694394ed6756068e7",
+    "url": "/zebulon-table/static/css/main.19be9d75.chunk.css"
+  },
+  {
+    "revision": "fdf694394ed6756068e7",
+    "url": "/zebulon-table/static/js/main.fdf69439.chunk.js"
+  },
+  {
+    "revision": "31055cb7f23c65793c18",
+    "url": "/zebulon-table/static/css/1.1b8a1487.chunk.css"
+  },
+  {
+    "revision": "31055cb7f23c65793c18",
+    "url": "/zebulon-table/static/js/1.31055cb7.chunk.js"
+  },
+  {
+    "revision": "460ea53b50ac20aedbc5",
+    "url": "/zebulon-table/static/js/runtime~main.460ea53b.js"
+  },
+  {
+    "revision": "e1e87e9d1ae1376c94406821153f40d8",
+    "url": "/zebulon-table/index.html"
+  }
+];
